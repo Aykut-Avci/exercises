2bb951abe98f8ec8971fd0c06ac2ac310ab8ee44
Aykut-Avci <aykutavci89@hotmail.com>
2022-08-24 17:44:26 +0200

    subqueries

--- lab_sql_join.sql
+++ lab_sql_join.sql
@@ -32,7 +32,7 @@ select first_name, last_name, address from staff inner join address using(addres
 -- Display the total amount rung up by each staff member in August of 2005.
 
 select sum(amount), first_name, last_name from payment left join staff using(staff_id)
-where payment_date between 050801 and 050831
+where payment_date between "2005-08-01" and "2005-08-31"
 group by staff_id;
 
 -- List each film and the number of actors who are listed for that film
@@ -41,16 +41,16 @@ select count(distinct actor_id), title from film_actor left join actor using(act
 group by film_id
 order by count(distinct actor_id) desc; 
 
--- Using the tables payment and customer and the JOIN command, list the total paid by each customer. List the customers alphabetically by last name
+-- 7. Using the tables payment and customer and the JOIN command, list the total paid by each customer. List the customers alphabetically by last name
 
-select first_name, last_name, sum(amount) from payment left join customer using(customer_id)
+select first_name, last_name, sum(amount) from payment join customer using(customer_id)
 group by customer_id
 order by last_name;
 
 
 -- Optional: Which is the most rented film? The answer is Bucket Brotherhood This query might require using more than one join statement. Give it a try.
 
-select title, count(film_id) from rental left join inventory using(inventory_id) left join film using(film_id)
+select title, count(film_id) from rental join inventory using(inventory_id) join film using(film_id)
 group by film_id
 order by count(film_id) desc
 
